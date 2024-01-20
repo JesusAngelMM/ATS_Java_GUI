214c669da70e89872c73973f3a7662e92234d943
JesúsÁngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-01-19 23:11:42 -0600

    13_4_Ventanas_JFrame

--- Ventana.java
+++ Ventana.java
@@ -1,13 +1,13 @@
-// Definición de la clase Ventana que hereda de JFrame
-
 import javax.swing.JFrame;
 
 public class Ventana extends JFrame {
 
-    // Constructor de la clase Ventana
     public Ventana() {
-        this.setSize(500, 500);  // Establecer el tamaño de la ventana (ancho x alto)
-        setDefaultCloseOperation(EXIT_ON_CLOSE); //Al cerrar la ventana el programa finaliza igual
-        setTitle("Este es un Titulo");  //Establcemos el titulo  
+        setSize(500, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE); 
+        setTitle("Este es un Titulo"); 
+        //setLocation(100, 200);
+        //setBounds(100, 200, 500, 500);
+        setLocationRelativeTo(null); // Establece la posición en el centro de la pantalla
     }
 }
