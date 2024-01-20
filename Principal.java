1241a68f375b287f98c170eeec17c7b1503fc8a5
JesúsÁngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-01-19 21:40:07 -0600

    13_2_Ventanas_JFrame

---
+++ Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+
+    // Método principal
+    public static void main(String[] args) {
+        Ventana v = new Ventana();  // Instanciar un objeto de la clase Ventana
+
+        // Por defecto, las ventanas son invisibles, así que la hacemos visible
+        v.setVisible(true);  // Hacer visible la ventana
+    }
+}
---
+++ Ventana.java
@@ -0,0 +1,11 @@
+// Definición de la clase Ventana que hereda de JFrame
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame {
+
+    // Constructor de la clase Ventana
+    public Ventana() {
+        this.setSize(500, 500);  // Establecer el tamaño de la ventana (ancho x alto)
+    }
+}
