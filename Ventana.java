75684ecf6283c4b10c5acddeb9774f56b64d0c01
JesúsÁngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-01-19 23:51:06 -0600

    13_11_Etiquetas_JLabel

--- Ventana.java
+++ Ventana.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -25,13 +26,20 @@ public class Ventana extends JFrame {
         panel.setLayout(null);  //Desactivamos el diseño por defecto
 
         JLabel etiqueta = new JLabel();  //Creamos una etiqueta
-        etiqueta.setText("Hola");  //Establecemos el texto de la etiqueta
+        etiqueta.setText("Mundial 2018");  //Establecemos el texto de la etiqueta
         etiqueta.setHorizontalAlignment(SwingConstants.CENTER);  //Alineamos
-        etiqueta.setBounds(10, 10, 70, 30);  //Modificamos ubicación
-        etiqueta.setForeground(Color.BLACK);
+        etiqueta.setBounds(85, 10, 300, 80);  //Modificamos ubicación
+        etiqueta.setForeground(Color.WHITE);
         etiqueta.setOpaque(true);  //Damos permiso de pintar la etiqueta
-        etiqueta.setBackground(Color.WHITE);  //Pintamos
+        etiqueta.setBackground(Color.BLACK);  //Pintamos
         etiqueta.setFont(new Font("Arial", Font.ITALIC, 20));   //Establecemos la fuente del texto
         panel.add(etiqueta);  //Agregamos la etiqueta al panel
+
+        //Etiqueta 2 - Etiqueta tipo imagen
+        JLabel etiquetaImg = new JLabel(new ImageIcon("imagen.jpg"));
+        etiquetaImg.setBounds(10, 100, 512, 640);
+
+        panel.add(etiquetaImg);
+
     }
 }
