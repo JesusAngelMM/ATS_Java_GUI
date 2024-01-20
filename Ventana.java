4701baf6fe359413f899ba3bca4147905abfb293
JesúsÁngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-01-19 23:18:50 -0600

    13_5_Ventanas_JFrame

--- Ventana.java
+++ Ventana.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Ventana extends JFrame {
@@ -8,6 +10,9 @@ public class Ventana extends JFrame {
         setTitle("Este es un Titulo"); 
         //setLocation(100, 200);
         //setBounds(100, 200, 500, 500);
-        setLocationRelativeTo(null); // Establece la posición en el centro de la pantalla
+        setLocationRelativeTo(null);  // Establecer en el centro
+        setResizable(false);  // Deshabilita la capacidad de redimensionar la ventana
+        setMinimumSize(new Dimension(200, 200));  // Establece el tamaño mínimo de la ventana
+        getContentPane().setBackground(Color.BLACK);  // Establece el color de fondo de la ventana
     }
 }
