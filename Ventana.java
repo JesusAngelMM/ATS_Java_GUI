570b8206213173133a354e8013192d029fb29081
JesúsÁngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-01-19 23:29:18 -0600

    13_7_Etiquetas_JLabel

--- Ventana.java
+++ Ventana.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Ventana extends JFrame {
@@ -11,13 +12,20 @@ public class Ventana extends JFrame {
         setTitle("Este es un Titulo"); 
         setLocationRelativeTo(null); 
         setMinimumSize(new Dimension(200, 200));  
-        getContentPane().setBackground(Color.BLACK);
+        //getContentPane().setBackground(Color.BLACK);
 
         iniciarComponentes();  //Colocamos sobre la Ventana
     }
     private void iniciarComponentes(){
         JPanel panel = new JPanel();  //Creamos el panel
-        panel.setBackground(Color.BLACK);
+        //panel.setBackground(Color.BLACK);
         this.getContentPane().add(panel);  //Agregamos el panel
+        panel.setLayout(null);  //Desactivamos el diseño
+
+        JLabel etiqueta = new JLabel();  //Creamos una etiqueta
+        etiqueta.setText("Hola");  //Establecemos el texto de la etiqueta
+        etiqueta.setBounds(10, 10, 50, 30);  //Modificamos ubicación
+
+        panel.add(etiqueta);  //Agregamos la etiqueta al panel
     }
 }
