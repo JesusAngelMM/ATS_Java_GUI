060d84b50142097d1595edca7c1e343d278db688
JesúsÁngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-01-19 23:08:37 -0600

    13_3_Ventanas_JFrame

--- Ventana.java
+++ Ventana.java
@@ -7,5 +7,7 @@ public class Ventana extends JFrame {
     // Constructor de la clase Ventana
     public Ventana() {
         this.setSize(500, 500);  // Establecer el tamaño de la ventana (ancho x alto)
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //Al cerrar la ventana el programa finaliza igual
+        setTitle("Este es un Titulo");  //Establcemos el titulo  
     }
 }
