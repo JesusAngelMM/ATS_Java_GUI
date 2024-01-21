e86ab358b4c4129e4e661c74b00e22f09ecf674c
JesúsÁngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-01-20 21:36:47 -0600

    13_13_Botones_JButton

--- Ventana.java
+++ Ventana.java
@@ -3,12 +3,15 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class Ventana extends JFrame {
+
+    public JPanel panel = new JPanel();
 
     public Ventana() {
         setSize(500, 500);
@@ -21,11 +24,19 @@ public class Ventana extends JFrame {
         iniciarComponentes();  //Colocamos sobre la Ventana
     }
     private void iniciarComponentes(){
-        JPanel panel = new JPanel();  //Creamos el panel
+        colocarPaneles();
+        //colocarEtiquetas();
+        colocarBotones();
+        
+    }
+    private void colocarPaneles(){
+        panel = new JPanel();  //Creamos el panel
         panel.setBackground(Color.BLACK);
         this.getContentPane().add(panel);  //Agregamos el panel
         panel.setLayout(null);  //Desactivamos el diseño por defecto
-
+    }
+    private void colocarEtiquetas(){
+        //Etiqueta 1 - etiqueta tipo texto
         JLabel etiqueta = new JLabel();  //Creamos una etiqueta
         etiqueta.setText("Mundial 2018");  //Establecemos el texto de la etiqueta
         etiqueta.setHorizontalAlignment(SwingConstants.CENTER);  //Alineamos
@@ -44,6 +55,15 @@ public class Ventana extends JFrame {
         etiquetaImg.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(etiquetaImg.getWidth(), etiquetaImg.getHeight(), Image.SCALE_SMOOTH)));
 
         panel.add(etiquetaImg);
-
+    }
+    private void colocarBotones(){
+        JButton boton1 = new JButton();  // Crear un botón
+    
+        boton1.setText("Click");  // Establecer un texto al botón
+        boton1.setBounds(100, 100, 100, 40);  // Tamaño y posición
+        boton1.setEnabled(true);  // Habilitar o deshabilitar el botón
+        boton1.setMnemonic('a');  // ALT + 'a' y funciona
+        
+        panel.add(boton1);  // Agregar al panel
     }
 }
